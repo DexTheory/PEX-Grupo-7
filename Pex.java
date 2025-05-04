bb8ab78435c5fe02bc9985fe5851f7423f0119ea
Gustavo Gomez A. <gtagomez@gmail.com>
2025-05-04 09:15:49 -0300

    PEX(updates)

---
+++ Pex.java
@@ -0,0 +1,22 @@
+public class Pex {
+
+    public static void main(String[] args) {
+
+    System.out.println("Seja Bem-Vindo ao Projeto de Extensão: " +
+
+                        "\n" +
+                        "   1. Introdução: Contextualize o projeto, explicando sua origem, importância e o problema que busca resolver.\n" +
+                        "\n" +
+                        "    2. Objetivos: Defina metas a curto e longo prazo, destacando os benefícios para a empresa ou/e comunidade.\n" +
+                        "\n" +
+                        "    3. Planejamento: Detalhe as etapas do projeto, ferramentas, recursos e tempo estimado para cada fase.\n" +
+                        "\n" +
+                        "    4. Execução: Descreva sua participação, tarefas realizadas, desafios enfrentados e como foram superados.\n" +
+                        "\n" +
+                        "    5. Resultados: Apresente resultados mensuráveis e marcos alcançados, como melhorias nas vendas ou gestão.\n" +
+                        "\n" +
+                        "    6. Impacto: Explique os benefícios para a empresa, clientes ou comunidade após a implementação.\n" +
+                        "\n" +
+                        "    7. Conclusão: Compartilhe aprendizados, conselhos e sugestões de melhorias ou próximos passos para o aplicativo.");
+    }
+}
